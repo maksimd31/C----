13cc636940d1e1ff0baed2856865f673b5651ffc
maksim31 <maksimdis31@gmail.com>
2022-08-08 17:25:01 +0300

    add number#5

--- git/C sharp/Lectures/Lecture № 2/Task №3/Program.cs
+++ git/C sharp/Lectures/Lecture № 2/Task №3/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*В данном варианте принимаются 3 аргумента 
+и функция страбатывает только при 3 элементах
+*/
+int Max(int a1, int a2, int a3)
+{
+    int numbers = a1;
+    if (a2>numbers) numbers = a2;
+    if (a3>numbers) numbers = a3;
+    return numbers;
+}
+
+//тип данны/имя/значения
+//. индек       0.  1.  2.  3 //{Скобки индекса}
+int[] array = {11, 22, 33, 333, 43534, 445, 4554, 5422, 2332};
+//array[0] = 12;
+Console.WriteLine(array[0]);
+//Вывод 12
+//Воспользуемся первой функиией 
+
+int max = Max(
+//  Вызов функицц   аргумент1  Аргумент2  Аргумент3
+    Max(array[0],array[1], array[2]),
+    Max(array[3],array[4], array[5]),
+    Max(array[6],array[7], array[8])
+);
+//покажет нам максимальное чмсло через индекс
+Console.WriteLine(max);
