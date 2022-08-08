23f984ce67a05734a2e3d0492e9308dacfe0503e
maksim31 <maksimdis31@gmail.com>
2022-08-08 16:19:01 +0300

    add number#2

---
+++ git/C sharp/Lectures/Lecture № 2/Task №2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Функции и методы 
+1. Блок с методом расчета, пишится один раз с верху
+*/
+int Max(int a1, int a2, int a3)
+{
+    int numders = a1;
+    if (a2>numders) numders = a2;
+    if (a3>numders) numders = a3;
+    return numders;
+}
+/*
+Обявление перемных и присваение к ним значений 
+*/
+int a1 = 12;
+int a2 = 111;
+int a3 = 333;
+/*
+Создаем список, в котором используем функцию Max. Функция Max прописывается с верхним ргистром 
+*/
+int numders = Max(a1, a2, a3);
+Console.WriteLine(numders);
+//С помощью метода  
