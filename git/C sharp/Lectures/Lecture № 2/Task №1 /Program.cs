671b62cc812fac68768a9ebbdaa16ebe7d4dee12
maksim31 <maksimdis31@gmail.com>
2022-08-08 16:05:44 +0300

    add number#1

---
+++ git/C sharp/Lectures/Lecture № 2/Task №1 /Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Пример № 1 Функции и методы 
+вычесление самого большого значения
+*/
+int a1 = 122;
+int a2 = 2;
+int a3 = 323;
+int a4 = 4;
+int a5 = 55;
+int a6 = 6;
+int a7 = 7;
+int a8 = 84;
+int a9 = 9;
+int max = a1;
+if (a2>max) max = a2;
+if (a3>max) max = a3;
+if (a4>max) max = a4;
+if (a5>max) max = a5;
+if (a6>max) max = a6;
+if (a7>max) max = a7;
+if (a8>max) max = a8;
+if (a9>max) max = a9;
+Console.WriteLine(max); 
+//Или создаем список и воспользуемся функицией 
