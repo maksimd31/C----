066f0c9df3d522e1001927bee0725c3fd55ef048
maksim31 <maksimdis31@gmail.com>
2022-08-08 16:58:12 +0300

    add number#3

--- git/C sharp/Lectures/Lecture № 2/Task №2/Program.cs
+++ git/C sharp/Lectures/Lecture № 2/Task №2/Program.cs
@@ -4,20 +4,39 @@
 */
 int Max(int a1, int a2, int a3)
 {
-    int numders = a1;
-    if (a2>numders) numders = a2;
-    if (a3>numders) numders = a3;
-    return numders;
+    int numbers = a1;
+    if (a2>numbers) numbers = a2;
+    if (a3>numbers) numbers = a3;
+    return numbers;
 }
 /*
 Обявление перемных и присваение к ним значений 
 */
-int a1 = 12;
-int a2 = 111;
-int a3 = 333;
+int a1 = 122;
+int a2 = 2;
+int a3 = 323;
+int a4 = 4;
+int a5 = 55;
+int a6 = 6;
+int a7 = 7;
+int a8 = 84;
+int a9 = 9;
 /*
 Создаем список, в котором используем функцию Max. Функция Max прописывается с верхним ргистром 
+Важно 3 списка создаем тольо потому что указали 3 арнумента!
 */
-int numders = Max(a1, a2, a3);
-Console.WriteLine(numders);
-//С помощью метода  
+int max1 = Max(a1, a2, a3);
+int max2 = Max(a4, a5, a6);
+int max3 = Max(a7, a8, a9);
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max);
+//С помощью метода
+/*
+//или
+int max = Max(
+    Max(a1,a2,a3),
+    Max(a4,a5,a6),
+    Max(a7,a8,a9)); 
+
+*/
