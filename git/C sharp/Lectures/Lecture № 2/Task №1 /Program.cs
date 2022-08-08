f3529e487e2572163ef11e96153320308df09468
maksim31 <maksimdis31@gmail.com>
2022-08-08 16:35:43 +0300

    add number#3

--- git/C sharp/Lectures/Lecture № 2/Task №1 /Program.cs
+++ git/C sharp/Lectures/Lecture № 2/Task №1 /Program.cs
@@ -11,15 +11,19 @@ int a6 = 6;
 int a7 = 7;
 int a8 = 84;
 int a9 = 9;
+
 int max = a1;
 //Если а2 Больше мах то а2 максимальное число а2
 if (a2>max) max = a2;
 if (a3>max) max = a3;
 if (a4>max) max = a4;
+
 if (a5>max) max = a5;
 if (a6>max) max = a6;
 if (a7>max) max = a7;
+
 if (a8>max) max = a8;
 if (a9>max) max = a9;
+
 Console.WriteLine(max); 
 //Или создаем список и воспользуемся функицией 
