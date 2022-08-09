8d3d2c080a5cfbc3bd8286f48cbacf1a34a1a472
maksim31 <maksimdis31@gmail.com>
2022-08-09 19:12:53 +0300

    add3

---
+++ git/C sharp/Homework/Homework№2/Lesson##2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Задание №2");
+
+Console.WriteLine("Напишите программу,которая выводит третью цифру заданного числа ил собщает что третьей цыфры нет. ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+string gg = Convert.ToString(number);
+if (gg.Length>1)
+{
+    Console.WriteLine("Третья цыфра числа " + gg[2]);
+}
+else if (gg.Length>2)
+{
+    Console.WriteLine("Третья цыфра числа " + gg[2]);
+}
+else if (gg.Length>4)
+{
+    Console.WriteLine("Ввод не сответсвует задаче!\nВведите трехзнвчное число! ");
+}
+else 
+{
+    Console.WriteLine("Ввод не сответсвует задаче!\nВведите трехзнвчное число! ");
+}
