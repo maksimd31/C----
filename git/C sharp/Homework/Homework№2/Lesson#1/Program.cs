ef6a3734535a50b2322eade8b284ee78a8b788ef
maksim31 <maksimdis31@gmail.com>
2022-08-09 08:48:43 +0300

    5

---
+++ git/C sharp/Homework/Homework№2/Lesson#1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая принимает не вход трехначное число и на выходе показывакет вторубю цифру этого числа.
+*/
+Console.WriteLine("Задание №1");
+Console.WriteLine("Напишите программу, которая принимает на вход трехначное число и на выходе показывакет вторубю цифру этого числа.");
+/*
+int[] arrey = Console.ReadLine("Введем трехзначное число ==> "());
+int n = arrey.Length;
+Console.WriteLine(n);
+*/
+int numb = Convert.ToInt32(Console.ReadLine());
+int[] arrey = {numb};
+int gg = arrey.Length;
+Console.WriteLine(gg);
+
+
+
---
+++ git/C sharp/Seminar/Lesson#2/Semenar2_project2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 11. Напишите программу,
+ которая выводит случайное трехзначное число и
+ удаляет вторую цифру этого числа
+*/
+
+int number = new Random().Next(100, 999);
+
+
+int number1 = number / 100 * 10;
+int number2 = number % 10;
+
+Console.Write(number);
+
+Console.WriteLine();
+
+Console.Write(number1 + number2);
