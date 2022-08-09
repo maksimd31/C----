3d329923287581f0909383651d12eac6d34c55e7
maksim31 <maksimdis31@gmail.com>
2022-08-09 18:30:07 +0300

    Well down#1

--- git/C sharp/Homework/Homework№2/Lesson#1/Program.cs
+++ git/C sharp/Homework/Homework№2/Lesson#1/Program.cs
@@ -1,30 +1,9 @@
-﻿/*
-Напишите программу, которая принимает не вход трехначное число и на выходе показывакет вторубю цифру этого числа.
-*/
+﻿
 Console.WriteLine("Задание №1");
 Console.WriteLine("Напишите программу, которая принимает на вход трехначное число и на выходе показывакет вторубю цифру этого числа.");
-
+//Ввод конвектируем в число
 int number = Convert.ToInt32(Console.ReadLine());
-
+//Если пытатся найти индекс с ввода через int то он выдаст одно значение, а если это сделать через значение строки то индекс будет считать каждую букву по этому конвектируем ввод в строку.
 string gg = Convert.ToString(number);
-
-Console.WriteLine("Вторая цифра числа: ",+gg[1]);
-
-/*
-int[] arrey = Console.ReadLine("Введем трехзначное число ==> "());
-int n = arrey.Length;
-Console.WriteLine(n);
-*/
-/*
-int numb = Convert.ToInt32(Console.ReadLine());
-int[] arrey = {numb};
-int gg = arrey.Length;
-Console.WriteLine(gg);
-
-
-
-
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
-*/
+//Выводим индекс Второго числа, Теперь не важно какое по длине будет число программа будет выдавать всегда второе число.
+Console.WriteLine("Вторая цифра числа: " + gg[1]);
