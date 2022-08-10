4b003892123ca40d81b96eb9f88252cffc498ac3
maksim31 <maksimdis31@gmail.com>
2022-08-10 18:12:47 +0300

    Дз №1 Готово

---
+++ git/C sharp/Homework/Homework№2/Lesson#1v2/Program.cs
@@ -0,0 +1,8 @@
+﻿//я считаю это решение не правельным 
+Console.WriteLine("Задание №1 v 2");
+Console.WriteLine("Напишите программу, которая принимает на вход трёхзначное число и на выходе показывакет вторую цифру этого числа.");
+int number = new Random().Next(100,999);
+Console.WriteLine($"===> {number}");
+int itogo = number % 100;
+int itogo2 = itogo/10;
+Console.WriteLine($"Вторая цыфра числа ==> {itogo2}");
