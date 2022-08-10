9eb4d56a916508a7da720cab56e10de3cc2ce443
maksim31 <maksimdis31@gmail.com>
2022-08-10 18:43:03 +0300

    Не работает

---
+++ git/C sharp/Homework/Homework№2/Lesson#2v2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Задание №2v2");
+
+Console.WriteLine("Напишите программу,которая выводит третью цифру заданного числа ил собщает что третьей цыфры нет. ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+string gg = Convert.ToString(number);
+if (gg.Length>1)
+{
+    Console.WriteLine("Третья цыфра числа " + gg[2]);
+}
+else if (gg.Length>2)
+{
+    Console.WriteLine("Третья цыфра числа " + gg[2]);
+}
+else if (gg.Length>4)
+{
+    Console.WriteLine("Ввод не сответсвует задаче!\nВведите трехзнвчное число! ");
+}
+else 
+{
+    Console.WriteLine("Ввод не сответсвует задаче!\nВведите трехзнвчное число! ");
+}
+//Не понимаю почему не работает второй вариант! При ввыводе значения else if консоль сходит сума.
