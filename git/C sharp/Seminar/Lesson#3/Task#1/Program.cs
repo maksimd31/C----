53985cc7b8e466a08194662bce9d029a3d706fdf
maksim31 <maksimdis31@gmail.com>
2022-08-12 08:31:56 +0300

    Дополнить код с семенара №3

---
+++ git/C sharp/Seminar/Lesson#3/Task#1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Код с семенара дополнить его с записи семенара.
+int getQuoterFromCoordinate(int x, int y )
+{
+    int resault = 0;
+    if (x>0 && y<0)
+    {
+        resault =1;
+    }
+    else if(x<0 && y >0)
+    {
+        resault = 2;
+    }
+    else if (x<0 && y < 0)
+    {
+        resault =3;
+    }
+    else if (x>0 && y < 0)
+    {
+        resault =4; 
+    }
+    else 
+    {
+        Console.ForegroundColor -ConsoleColor.Red;
+        Console.WriteLine($"X и Y Не должны быть равны 0 вы ввели {x} {Y}");
+        Console.ResetColor();
+    }
+    return resault;
+}
+int userX =0;
+int userY = 0;
+
+Console.WriteLine("Введите Х: ");
+userX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y: ");
+userY = Convert.ToInt32(Console.ReadLine());
+int quoter = getQuoterFromCoordinate;
