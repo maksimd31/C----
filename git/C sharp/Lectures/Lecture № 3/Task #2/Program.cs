e0d5346b7dde1d8347a38d1f040832904c6a28e5
maksim31 <maksimdis31@gmail.com>
2022-08-14 16:50:06 +0300

    add4

---
+++ git/C sharp/Lectures/Lecture № 3/Task #2/Program.cs
@@ -0,0 +1,14 @@
+﻿//этот метод печатает склеенный текст 10 раз 
+string Metod4(int count, string text )
+{
+    int i=0; //счетчик 
+    string result = string.Empty; // у переменной result строка и объявляем что строка пустая! 
+    while (i<count)// цикл пока i меньше count цикл будет выполнятся 
+    {
+        result = result + text; //происходит склеивание  
+        i++;
+    }
+    return result; // останавливаем цикл при достижении цели
+}
+string res = Metod4(10, "qwerty"); //Объявляем метод 
+Console.WriteLine(res); //Выводим метод 
