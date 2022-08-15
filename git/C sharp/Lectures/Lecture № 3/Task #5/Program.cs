bec858e061b15cdfe3a124837df8090265b44971
maksim31 <maksimdis31@gmail.com>
2022-08-15 10:00:30 +0300

    add#1

--- git/C sharp/Lectures/Lecture № 3/Task #5/Program.cs
+++ git/C sharp/Lectures/Lecture № 3/Task #5/Program.cs
@@ -1,4 +1,35 @@
 ﻿/*
 Упорядочить массив 
 */
-//array = {};
+int[] arr = {7, 5, 3, 4, 5, 6, 7, 7, 7};
+
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+//метод растравление массива 
+void SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1 ; i++)
+    {
+        int minPosition = i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) 
+            minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+
+
+SelectionSort(arr);
