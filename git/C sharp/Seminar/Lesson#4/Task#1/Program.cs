d0a0f759c93b099ab67f25da5c8348a2740216c4
maksim31 <maksimdis31@gmail.com>
2022-08-15 21:57:33 +0300

    add#11

--- git/C sharp/Seminar/Lesson#4/Task#1/Program.cs
+++ git/C sharp/Seminar/Lesson#4/Task#1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int factorial(int number)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+Console.WriteLine($"Факториал {factorial(num)}");
