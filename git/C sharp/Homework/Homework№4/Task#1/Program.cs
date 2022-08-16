c3e0ed5f21bcf2d60ad4be6c26189f009f8ea2bf
maksim31 <maksimdis31@gmail.com>
2022-08-16 18:31:42 +0300

    г2

--- git/C sharp/Homework/Homework№4/Task#1/Program.cs
+++ git/C sharp/Homework/Homework№4/Task#1/Program.cs
@@ -4,6 +4,7 @@
 2, 4 -> 16
 */
 //Решение задачи(возведение в степень)
+Console.Clear();
 Console.WriteLine("Задача 25:\nНапишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.");
 
 Console.Write("Введите число А: ");
@@ -15,3 +16,5 @@ int numB = Convert.ToInt32(Console.ReadLine());
 int sum = Convert.ToInt32(Math.Pow(numA, numB)); 
 
 Console.WriteLine($"Число А = {numA} в натуральной степени B = {numB}\nРавняется = {sum} ");
+//Math.Pow - функция возведение в степень работает изначально только с значениями double!
+//Что бы сработало нужно Convert.ToInt32 в int
