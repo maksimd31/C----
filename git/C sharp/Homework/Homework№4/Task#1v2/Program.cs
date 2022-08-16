f97c3b349b3fd10667caef9380d6c5a8a80c66c6
maksim31 <maksimdis31@gmail.com>
2022-08-16 19:18:43 +0300

    г3

--- git/C sharp/Homework/Homework№4/Task#1v2/Program.cs
+++ git/C sharp/Homework/Homework№4/Task#1v2/Program.cs
@@ -13,10 +13,17 @@ int numA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B: ");
 int numB = Convert.ToInt32(Console.ReadLine());
 
-int sum = Convert.ToInt32(Math.Pow(numA, numB)); 
+int sum(int numB, int numA)//метод
+{
+    int number1 = numA;
+    for(int i = 0; i<numB; i++) 
+    {
+        number1 *= numA;
+    }
+    return number1;
+}
 
-Console.WriteLine($"Число А = {numA} в натуральной степени B = {numB}\nРавняется = {sum} ");
-//Math.Pow - функция возведение в степень работает изначально только с значениями double!
-//Что бы сработало нужно Convert.ToInt32 в int
+Console.WriteLine($"Число А = {numA} в натуральной степени B = {numB}\nРавняется = {sum(numA,numB)} ");
+
 
 
--- git/C sharp/Seminar/Lesson#4/test/Program.cs
+++ git/C sharp/Seminar/Lesson#4/test/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16*/
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int numOne = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numTwo = int.Parse(Console.ReadLine());
+
+int getRate(int numberOne, int numberTwo)
+{
+    int totalNum = numOne;
+    for(int i = 1; i < numTwo; i++) {
+        totalNum *= numOne;
+    }
+    return totalNum;
+}
+
+Console.WriteLine($"Результат = {getRate(numOne, numTwo)}");
