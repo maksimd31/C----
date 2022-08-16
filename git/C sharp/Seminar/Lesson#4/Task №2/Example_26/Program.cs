6c90b8778b8ef7d830bcab2dc66151f3a67084f2
maksim31 <maksimdis31@gmail.com>
2022-08-16 18:01:52 +0300

    г

--- git/C sharp/Seminar/Lesson#4/Task №2/Example_26/Program.cs
+++ git/C sharp/Seminar/Lesson#4/Task №2/Example_26/Program.cs
@@ -9,13 +9,13 @@
 
 */
 
-int HowMuchIsTheDigits(int number)
+int HowMuchIsTheDigits(int number) //метод 
 {
-    int digitCounter = 0;
-    while (number != 0)
+    int digitCounter = 0; //счетчик 
+    while (number != 0) //цикл Сравнивает два операнда и возвращает true, если операнды не равны, и false, если они равны.
     {
-        number = number / 10;
-        digitCounter++;
+        number = number / 10;//вводную цифру делим на 10 
+        digitCounter++; // +1 к переменной 
         //Console.WriteLine(number);
         //Console.WriteLine(digitCounter);
     }
@@ -24,5 +24,7 @@ int HowMuchIsTheDigits(int number)
 
 
 Console.WriteLine("Введите число");
+
 int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
 Console.WriteLine($"Количество цифр в числе {number} = {HowMuchIsTheDigits(number)}");
