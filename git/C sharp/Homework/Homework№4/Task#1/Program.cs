3c32306cc2fc84cf8e628b07f1d150543c2effd9
maksim31 <maksimdis31@gmail.com>
2022-08-16 18:23:08 +0300

    Д/З готово

--- git/C sharp/Homework/Homework№4/Task#1/Program.cs
+++ git/C sharp/Homework/Homework№4/Task#1/Program.cs
@@ -3,3 +3,15 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16
 */
+//Решение задачи(возведение в степень)
+Console.WriteLine("Задача 25:\nНапишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.");
+
+Console.Write("Введите число А: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int sum = Convert.ToInt32(Math.Pow(numA, numB)); 
+
+Console.WriteLine($"Число А = {numA} в натуральной степени B = {numB}\nРавняется = {sum} ");
