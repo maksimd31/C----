8e2845ac0f5acc1e822bc50ca0f3cee897b3e89d
maksim31 <maksimdis31@gmail.com>
2022-08-17 17:12:37 +0300

    g2

--- git/C sharp/Homework/Homework№4/Task#2/Program.cs
+++ git/C sharp/Homework/Homework№4/Task#2/Program.cs
@@ -11,40 +11,19 @@ Console.WriteLine("Задача 27:\nНапишите программу, кот
 
 Console.Write("Введите число: ==> ");
 int numA = Convert.ToInt32(Console.ReadLine());
-/*
-int Summma(int numA)
-{
-    //int summ = numA;
-    for(int i = 0; i<numA; i++)
-    {
-        Console.WriteLine(i);
-        numA += numA;
-        Console.WriteLine(numA);
-    }
-    return numA;
-}
-Console.WriteLine($"Введенное число  = {numA}\nсумма его цифр равняется  = {Summma(numA)}");
-*/
-/*
-foreach (var i in Convert.ToString(numA))
-{
-    if (i<numA)
-    {
-        numA += numA;
-        i++;
-        Console.WriteLine(numA);
-        Console.WriteLine(i);
-    }
-    //Console.Write(c);
-    //int i = Convert.ToInt32(c);
-    //Console.WriteLine(i);
-    //for(i = 1; i<numA; i++)
-    {
-        //Console.WriteLine(numA);
 
+int tSumm(int numA)
+{
+    int s = 0;
+    while (numA > 0) //
+    {
+        s = s + numA % 10;//
+        numA = numA /10 ;
     }
-    
+    return s;
 }
-*/
+
+Console.WriteLine($"Сумма цифр {numA}\nРавна = {tSumm(numA)}");
+
 
 
