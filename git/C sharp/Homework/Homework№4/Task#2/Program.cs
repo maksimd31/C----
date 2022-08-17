9489a50d7aa37e103469c69feac292f869391946
maksim31 <maksimdis31@gmail.com>
2022-08-17 16:43:34 +0300

    г6

--- git/C sharp/Homework/Homework№4/Task#2/Program.cs
+++ git/C sharp/Homework/Homework№4/Task#2/Program.cs
@@ -4,3 +4,47 @@
 82 -> 10
 9012 -> 12
 */
+
+//Решение задачи(возведение в степень)
+Console.Clear();
+Console.WriteLine("Задача 27:\nНапишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
+
+Console.Write("Введите число: ==> ");
+int numA = Convert.ToInt32(Console.ReadLine());
+/*
+int Summma(int numA)
+{
+    //int summ = numA;
+    for(int i = 0; i<numA; i++)
+    {
+        Console.WriteLine(i);
+        numA += numA;
+        Console.WriteLine(numA);
+    }
+    return numA;
+}
+Console.WriteLine($"Введенное число  = {numA}\nсумма его цифр равняется  = {Summma(numA)}");
+*/
+/*
+foreach (var i in Convert.ToString(numA))
+{
+    if (i<numA)
+    {
+        numA += numA;
+        i++;
+        Console.WriteLine(numA);
+        Console.WriteLine(i);
+    }
+    //Console.Write(c);
+    //int i = Convert.ToInt32(c);
+    //Console.WriteLine(i);
+    //for(i = 1; i<numA; i++)
+    {
+        //Console.WriteLine(numA);
+
+    }
+    
+}
+*/
+
+
