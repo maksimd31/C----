450e669d3e6fbbf53a5e1a0fc5e205ba10ff79b5
maksim31 <maksimdis31@gmail.com>
2022-08-23 13:09:20 +0300

    version3

---
+++ Seminar/Lesson#5/Task#35/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задание №35
+// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+
+int[] arr = new int[123]; // создали массив 
+
+void RandomArray(int[] array)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        //Создание объекта для генерации чисел
+        Random rnd = new Random();
+        //Получить случайное число 
+        int value = rnd.Next(1000);
+        arr[i] = value;
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+void IsNumInArray(int[] array)
+{
+    int numExist = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 10 && arr[i] < 99)
+        {
+            numExist++;
+        }
+    }
+    if (numExist > 0)
+    {
+        Console.WriteLine($"Количество чисел - {numExist}");
+    }
+    else
+    {
+        Console.WriteLine($"Чисел нет - {numExist}");
+    }
+}
+
+RandomArray(arr);
+IsNumInArray(arr);
