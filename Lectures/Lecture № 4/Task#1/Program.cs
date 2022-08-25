c6eaa8b17acfa74b853493a89f479c76c8cd9cb9
maksim31 <maksimdis31@gmail.com>
2022-08-25 18:55:26 +0300

    version7

--- Lectures/Lecture № 4/Task#1/Program.cs
+++ Lectures/Lecture № 4/Task#1/Program.cs
@@ -9,7 +9,7 @@
 // GetLength(0)//количество строк  
 
 // string[,] tab =new string[10, 10]; //таблица строк
-string[,] table = new string[2,5];
+string[,] table = new string[2, 5];
 // String.Empty
 // table [0,0] table [0,1] table [0,2] table [0,4]
 // table [1,0] table [1,1] table [1,2] table [1,4]
@@ -23,17 +23,18 @@ void PrintArray(int[,] matr)
         {
             Console.Write($"{matr[i, j]} ");
         }
-    Console.WriteLine();
-} }
+        Console.WriteLine();
+    }
+}
 void FillArray(int[,] matr)
 {
     for (int i = 0; i < matr.GetLength(0); i++)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            matr[i,j] = new Random().Next(1,10);//[1; 10)
+            matr[i, j] = new Random().Next(1, 10);//[1; 10)
         }
-}
+    }
 }
 int[,] matrix = new int[3, 4];
 PrintArray(matrix);
