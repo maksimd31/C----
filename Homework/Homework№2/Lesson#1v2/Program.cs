9ecf65c429b439170305ac4b4b0a5a7d17c0c4b5
maksim31 <maksimdis31@gmail.com>
2022-08-25 19:11:50 +0300

    version8

--- Homework/Homework№2/Lesson#1v2/Program.cs
+++ Homework/Homework№2/Lesson#1v2/Program.cs
@@ -1,9 +1,15 @@
 ﻿//я считаю это решение не правальным 
+/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
 Console.WriteLine("Задание №1 v 2");
 Console.WriteLine("Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
-int number = new Random().Next(100,999);
+int number = new Random().Next(100, 999);
 Console.WriteLine($"===> {number}");
-int itogo = number % 100;
-Console.WriteLine($"===> {itogo}");
-int itogo2 = itogo/10;
-Console.WriteLine($"Вторая цыфра числа ==> {itogo2}");
+int Togo = number % 100;
+Console.WriteLine($"===> {Togo}");
+int Togo2 = Togo / 10;
+Console.WriteLine($"Вторая цифра числа ==> {Togo2}");
--- Homework/Homework№2/Lesson#3/Program.cs
+++ Homework/Homework№2/Lesson#3/Program.cs
@@ -1,28 +1,34 @@
 ﻿
+/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
 Console.WriteLine("Задача №3\nНапишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
-//for (int i = 0; i<7; i++); Не монимаю как запустить 7 раз ?
+//for (int i = 0; i<7; i++); Не понимаю как запустить 7 раз ?
 int number = Convert.ToInt32(Console.ReadLine());
 switch (number)
 {
     case 1:
-    Console.WriteLine("Будний день - Понедельник");
-    break;
+        Console.WriteLine("Будний день - Понедельник");
+        break;
     case 2:
-    Console.WriteLine("Будний день - Вторник");
-    break;
+        Console.WriteLine("Будний день - Вторник");
+        break;
     case 3:
-    Console.WriteLine("Будний день - Среда");
-    break;
+        Console.WriteLine("Будний день - Среда");
+        break;
     case 4:
-    Console.WriteLine("Будний день - Четверг");
-    break;
+        Console.WriteLine("Будний день - Четверг");
+        break;
     case 5:
-    Console.WriteLine("Будний день - Пятница");
-    break;
+        Console.WriteLine("Будний день - Пятница");
+        break;
     case 6:
-    Console.WriteLine("Ура выходной - Суббота");
-    break;
+        Console.WriteLine("Ура выходной - Суббота");
+        break;
     case 7:
-    Console.WriteLine("Ура выходной - Воскресенье");
-    break;
+        Console.WriteLine("Ура выходной - Воскресенье");
+        break;
 }
