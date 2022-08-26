91035ef20d11e329f52275d005c03b5a2817fb83
maksim31 <maksimdis31@gmail.com>
2022-08-26 17:25:11 +0300

    version24

--- Homework/Homework№6/Task#42/Program.cs
+++ Homework/Homework№6/Task#42/Program.cs
@@ -5,7 +5,7 @@ x = k1 * x + b1, y = k2 * x + b2;
 значения b1, k1, b2 и k2 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
-Console.Clear();
+//Console.Clear();
 
 Console.Write("введите значение b1: ");
 double b1 = Convert.ToInt32(Console.ReadLine());
@@ -22,11 +22,28 @@ double k2 = Convert.ToInt32(Console.ReadLine());
 double x = (-b2 + b1) / (-k1 + k2);
 double y = k2 * x + b2;
 
-if (b1==b2) Console.WriteLine("b1 Прямые не пересекаются b2 ");
-if (k1==k2) Console.WriteLine("k1 Прямые не пересекаются k2 ");
-else
+while (b1 > 0)
 {
-    Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+    if (b1 == b2)
+    {
+        Console.WriteLine("b1 Прямые не пересекаются b2 ");
+        
+    }
+    else
+    {
+        Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+        break;
+    }
+    if (k1 == k2)
+    {
+        Console.WriteLine("k1 Прямые не пересекаются k2 ");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+        break;
+    }
 }
 
 
