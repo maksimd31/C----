2dc9a22f18048c9a9cbbf4ea9af77fc7a44da9ed
maksim31 <maksimdis31@gmail.com>
2022-09-02 17:01:24 +0300

    v9

--- Homework/Homework№8/Task#54/Program.cs
+++ Homework/Homework№8/Task#54/Program.cs
@@ -7,3 +7,67 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+
+Console.Clear();
+Console.WriteLine("Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.\nВведите размер массива m x n и диапазон случайных значений: ");
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите диапазон: от 1 до => ");
+int range = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n]; //Массив 
+CreateArray(array);
+WriteArray(array);
+
+Console.WriteLine($"\nОтсортированный массив: "); //Сортировка 
+OrderArrayL(array);
+WriteArray(array);
+
+void OrderArrayL(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
