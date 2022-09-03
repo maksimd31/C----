df78be2284b377c650bdd4b6dd4b58e89db4c1b9
maksim31 <maksimdis31@gmail.com>
2022-09-03 13:25:07 +0300

    v10

--- Homework/Homework№8/Task#56/Program.cs
+++ Homework/Homework№8/Task#56/Program.cs
@@ -5,3 +5,69 @@
 // 8 4 2 4
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.Clear();
+
+Console.WriteLine($"Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.\nВведите размер массива m x n и диапазон случайных значений:");
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите диапазон: от 1 до => ");
+int range = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+CArray(array);
+WArray(array);
+
+int minSumLine = 0;
+int sumLin = SumElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = SumElements(array, i);
+  if (sumLin > tempSumLine)
+  {
+    sumLin = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строка с наименьшей суммой ({sumLin}) элементов ");
+
+
+int SumElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+
+void CArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
